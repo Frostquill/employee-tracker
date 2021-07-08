d73133a874ad0fada1ed1d20a4a18c4f5e035981
Frostquill <ruiz82494@gmail.com>
2021-07-07 18:14:40 -0700

    inquirer created with methods started

--- db/schema.sql
+++ db/schema.sql
@@ -2,19 +2,30 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
-CREATE TABLE employee (
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER,
-    manager_id INTEGER
-);
 
-CREATE TABLE role(
-    title VARCHAR (30),
-    salary DECIMAL, 
-    department_id INTEGER
-);
+
+
 
 CREATE TABLE department (
-    name VARCHAR(30)
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE role(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL(10,0), 
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
